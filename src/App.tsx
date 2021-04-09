018e260f4bf9e64b253796807abcf3403d4d7c4d
Angele <angelealst@hotmail.com>
2021-04-09 18:31:11 -0300

    #melhorando a pesquisa

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ interface cepData{
 
 function App() {
     const [cepOutput, setCepOutput ] = useState<cepData>();
-    const [cepInput, setCepInput] = useState<any>(null);
+    const [cepInput, setCepInput] = useState('');
 
     const handleChangeCep=(event: { target: { value: string; }; })=>{
         if(!(event.target.value.length===0)){
@@ -41,62 +41,69 @@ function App() {
                             <Form.Label>Digite um CEP para consultar o endereço:</Form.Label>
                         </Row>
                         <Row>
-                            <input type="textarea" id="input_cep" onChange={handleChangeCep}/>
+                            <input id="input_cep" onChange={handleChangeCep}/>
                         </Row>
                         <br/>
                         <Row>
-                            <Button id="consultar" type="button" onClick={handleInput}>Consultar</Button>
+                            <Button id="consultar" type="submit" onClick={handleInput}>Consultar</Button>
                         </Row>
                         <br/>
                     </Form.Group>
                 </Form>
             </Row>
         </Container>
-        <Container>
-            <Row id="cep-list" >
-                <Table>
-                    <thead>Endereço completo</thead>
-                    <tr>
-                        <th>Cep</th>
-                        <th> {cepOutput?.cep}</th>
-                    </tr>
-                    <tr>
-                        <th>
-                            Logradouro
-                        </th>
-                        <th>{cepOutput?.logradouro}</th>
-                    </tr>
-                    <tr>
-                        <th>Complemento</th>
-                        <th>{cepOutput?.complemento}</th>
-                    </tr>
-                    <tr>
-                        <th>Bairro</th>
-                        <th>{cepOutput?.bairro}</th>
-                    </tr>
-                    <tr>
-                        <th>UF</th>
-                        <th>{cepOutput?.uf}</th>
-                    </tr>
-                    <tr>
-                        <th>IBGE</th>
-                        <th>{cepOutput?.ibge}</th>
-                    </tr>
-                    <tr>
-                        <th>GIA</th>
-                        <th>{cepOutput?.gia}</th>
-                    </tr>
-                    <tr>
-                        <th>DDD</th>
-                        <th>{cepOutput?.ddd}</th>
-                    </tr>
-                    <tr>
-                        <th>SIAFI</th>
-                        <th>{cepOutput?.siafi}</th>
-                    </tr>
-                </Table>
+        { cepOutput ? (
+            <Container>
+                <Row id="cep-list" >
+                    <Table>
+                        <thead>Endereço completo</thead>
+                        <tr>
+                            <th>Cep</th>
+                            <th> {cepOutput?.cep}</th>
+                        </tr>
+                        <tr>
+                            <th>
+                                Logradouro
+                            </th>
+                            <th>{cepOutput?.logradouro}</th>
+                        </tr>
+                        <tr>
+                            <th>Complemento</th>
+                            <th>{cepOutput?.complemento}</th>
+                        </tr>
+                        <tr>
+                            <th>Bairro</th>
+                            <th>{cepOutput?.bairro}</th>
+                        </tr>
+                        <tr>
+                            <th>UF</th>
+                            <th>{cepOutput?.uf}</th>
+                        </tr>
+                        <tr>
+                            <th>IBGE</th>
+                            <th>{cepOutput?.ibge}</th>
+                        </tr>
+                        <tr>
+                            <th>GIA</th>
+                            <th>{cepOutput?.gia}</th>
+                        </tr>
+                        <tr>
+                            <th>DDD</th>
+                            <th>{cepOutput?.ddd}</th>
+                        </tr>
+                        <tr>
+                            <th>SIAFI</th>
+                            <th>{cepOutput?.siafi}</th>
+                        </tr>
+                    </Table>
+                </Row>
+            </Container>
+        ) : (
+            <Row className="justify-content-md-center">
+                Nada pesquisado
             </Row>
-        </Container>
+        )}
+
     </div>
   );
 }
